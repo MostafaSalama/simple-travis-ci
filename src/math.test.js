476a7e6fbb210a9f394ca43e3b496808e4d104cf
Mostafa Salama <mostafatourad789@gmail.com>
2020-07-25 14:00:06 +0200

    add test for abs function

--- src/math.test.js
+++ src/math.test.js
@@ -1,5 +1,8 @@
 const math = require('./math') ;
 
-test('it should get the correct square of the value',()=>{
+test('it should get the correct power of the value',()=>{
     expect(math.power(2,3)).toBe(8);
+})
+test('it should get absolute value',()=>{
+    expect(math.abs(-1)).toBe(1);
 })
